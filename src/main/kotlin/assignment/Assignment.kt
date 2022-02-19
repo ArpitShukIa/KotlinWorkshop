4cd36cde0234ab6e98b6f70c6d1232cad9807be5
Adrito-M <adrito.mukherjee.cse21@itbhu.ac.in>
2022-02-19 23:46:08 +0530

    added functionality

--- src/main/kotlin/assignment/Assignment.kt
+++ src/main/kotlin/assignment/Assignment.kt
@@ -1,4 +1,7 @@
-package assignment
+import java.util.Scanner
+import java.io.File
+
+// package assignment
 
 // INSTRUCTIONS:
 // 1. Read a list of student responses from text file
@@ -21,13 +24,45 @@ fun main() {
 }
 
 fun readResponses(): List<Response> {
-    TODO()
+    val returnList = mutableListOf<Response>()
+    val fileContent = File("responses.txt")
+    val sc = Scanner(fileContent)
+    sc.nextLine()
+
+    var name: String
+    var rollNumber: Int
+    var branch: String
+    var covidPositive: String
+
+    while (sc.hasNextLine()) {
+        sc.next()
+        name = sc.next()
+        rollNumber = sc.nextInt()
+        branch = sc.next()
+        covidPositive = sc.next()
+        if (covidPositive == "no")
+            returnList.add(Response(name, rollNumber, branch, false));
+    }
+    sc.close()
+    returnList.sortWith(compareBy<Response> {it.branch}.thenBy {it.name})
+    return returnList.toList()
 }
 
-fun processResponses(responses: List<Response>): List<List<Response>> {
-    TODO()
+fun processResponses(responses: List<Response>): List<List<Response>> {   
+    
+    return responses.groupBy {it.branch}.values.toList().withIndex().groupBy { it.index / 3 }.map { it.value.flatMap { it.value } }
+
 }
 
 fun writeToFile(batches: List<List<Response>>) {
-    TODO()
+
+    File("batches.txt").printWriter().use { out ->
+
+        for ((index, batch) in batches.withIndex()) {
+            out.println("Batch ${index+1}:")
+            for ((studentIndex, student) in batch.withIndex())
+                out.println("${studentIndex+1}.\t${student.name}\t\t${student.rollNumber}\t\t${student.branch}")
+            out.println()
+        }
+    }
 }
