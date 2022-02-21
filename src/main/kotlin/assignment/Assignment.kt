e2dfc6faa47bfde0ed6047442cec45bfdf6bcddb
shriya-tiwari <shriya.tiwari.cse21@itbhu.ac.in>
2022-02-21 19:30:46 +0530

    Completed Task

--- src/main/kotlin/assignment/Assignment.kt
+++ src/main/kotlin/assignment/Assignment.kt
@@ -1,5 +1,8 @@
 package assignment
 
+import java.io.File
+import java.io.InputStream
+
 // INSTRUCTIONS:
 // 1. Read a list of student responses from text file
 // 2. Filter students who are not covid positive
@@ -21,13 +24,31 @@ fun main() {
 }
 
 fun readResponses(): List<Response> {
-    TODO()
+    val inputStream : InputStream = File("src/main/kotlin/assignment/responses.txt").inputStream()
+    val inputString : List<String> = inputStream.bufferedReader().use { it.readLines() }
+    val responses = mutableListOf<Response>()
+    for (i in 1 until inputString.size){
+        val eachline = inputString[i].splitToSequence(' ').filter{it.isNotEmpty()}.filter { it.isNotEmpty() }.toList()
+        responses.add(Response(eachline[1], eachline[2].toInt(), eachline[3], eachline[4]=="yes"))
+    }
+    return responses
 }
 
 fun processResponses(responses: List<Response>): List<List<Response>> {
-    TODO()
+    return responses.filter { !it.covidPositive }.sortedWith(compareBy({ it.branch }, { it.name })).groupBy { it.branch }.values.chunked(3)
+        .map { it.flatten() }
 }
 
 fun writeToFile(batches: List<List<Response>>) {
-    TODO()
+    File("src/main/kotlin/assignment/batches.txt").bufferedWriter().use { out ->
+        for((index, value) in batches.withIndex()) {
+            out.write("Batch ${index+1}:")
+            out.newLine()
+            for((studIndex, student) in value.withIndex()){
+                out.write("${studIndex+1}\t\t\t${student.name}\t\t\t${student.branch}\t\t\t${student.rollNumber}")
+                out.newLine()
+            }
+            out.newLine()
+        }
+    }
 }
